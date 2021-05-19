a3ff7e58f0c8afddbf1997646fd15d4d51f30139
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-19 15:07:24 +1200

    Start GUI Set-up - Video
    End of video.

--- 02_start_GUI_v1.py
+++ 02_start_GUI_v1.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from functools import partial  # To prevent unwanted windows
+from functools import partial   # To prevent unwanted windows
 import random
 
 
@@ -13,16 +13,50 @@ class Start:
         # Mystery Heading (row 0)
         self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
                                        font="Arial 19 bold")
-        self.mystery_box_label.grid(row=1)
+        self.mystery_box_label.grid(row=0)
 
-        # Entry box... (row 1)
-        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        # Initial Instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame, font="Arial 10 italic",
+                                          text="Please enter a dollar amount "
+                                               "(between $5 and $50) in the box "
+                                               "below.  Then choose the the "
+                                               "stakes.  The higher the stakes, "
+                                               "the more you can win!",
+                                          wrap=275, justify=LEFT, padx=10, pady=10)
+        self.mystery_instructions.grid(row=1)
+
+        # Entry box (row 2)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 19 bold")
         self.start_amount_entry.grid(row=2)
 
-        # Play Button (row 2)
-        self.lowstakes_button = Button(text="Low ($5)",
-                                       command=lambda: self.to_game(1))
-        self.lowstakes_button.grid(row=2, pady=10)
+        # button frame (row 3)
+        self.stakes_frame = Frame(self.start_frame)
+        self.stakes_frame.grid(row=3)
+
+        # Buttons go here...
+        button_font = "Arial 12 bold"
+        # Orange low stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
+                                       command=lambda: self.to_game(1),
+                                       font=button_font, bg="#FF9933")
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
+
+        # Yellow medium stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+                                       command=lambda: self.to_game(2),
+                                       font=button_font, bg="#FFFF33")
+        self.lowstakes_button.grid(row=0, column=1, padx=5, pady=10)
+
+        # Green high stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="High ($15)",
+                                       command=lambda: self.to_game(3),
+                                       font=button_font, bg="#99FF33")
+        self.lowstakes_button.grid(row=0, column=2, pady=10)
+
+        # Help Button
+        self.help_button = Button(self.start_frame, text="How to Play",
+                                  bg="#808080", fg="white", font=button_font)
+        self.help_button.grid(row=4, pady=10)
 
     def to_game(self, stakes):
         starting_balance = self.start_amount_entry.get()
@@ -39,6 +73,6 @@ class Game:
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("Mystery Box")
-    start_program = Start(root)
+    root.title("Mystery Box Game")
+    something = Start(root)
     root.mainloop()
