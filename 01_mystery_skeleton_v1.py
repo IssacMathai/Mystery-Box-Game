c2e819e3ce188c84d79f3607fabab3afce046810
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-19 14:34:04 +1200

    Halfway into Mystery Box Set Up - Video

---
+++ 01_mystery_skeleton_v1.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Entry box... (row 1)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=2)
+
+        # Play Button (row 2)
+        self.lowstakes_button = Button(text="Low ($5)",
+                                       command=lambda: self.to_game(1))
+        self.lowstakes_button.grid(row=2, pady=10)
+
+    def to_game(self, stakes):
+        starting_balance = self.start_amount_entry.get()
+        Game(self, stakes, starting_balance)
+
+
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
+
+        partner.lowstakes_button.config(state=DISABLED)
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box")
+    start_program = Start(root)
+    root.mainloop()
