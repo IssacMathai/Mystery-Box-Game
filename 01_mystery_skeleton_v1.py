27285ccf11a3b03c3974ad0ac79715b15dcce84e
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-19 14:53:29 +1200

    Mystery Box Set Up - Video
    Code at the end of the video.

--- 01_mystery_skeleton_v1.py
+++ 01_mystery_skeleton_v1.py
@@ -34,7 +34,51 @@ class Game:
         print(stakes)
         print(starting_balance)
 
+        # disable low stakes button
         partner.lowstakes_button.config(state=DISABLED)
+
+        # initialise variables
+        self.balance = IntVar()
+
+        # Set starting balance to amount entered by user at start of game
+        self.balance.set(starting_balance)
+
+        # GUI Setup
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # Heading Row
+        self.heading_label = Label(self.game_frame, text="Heading",
+                                   font="Arial 24 bold", padx=10,
+                                   pady=10)
+        self.heading_label.grid(row=0)
+
+        # Balance Label
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, text="Balance...")
+        self.balance_label.grid(row=2)
+
+        self.play_button = Button(self.game_frame, text="Gain",
+                                  padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=3)
+
+    def reveal_boxes(self):
+        # retrieve the balance from the initial function...
+        current_balance = self.balance.get()
+
+        # Adjust the balance (subtract game cost and add pay out)
+        # For testing purposes, just add 2
+        current_balance += 2
+
+        # Set balance to adjusted balance
+        self.balance.set(current_balance)
+
+        # Edit label so user can see their balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
+        
 
 # main routine
 if __name__ == "__main__":
