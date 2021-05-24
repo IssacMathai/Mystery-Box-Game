d6c0beb285e64e8b7b6377ae759eff80585d9208
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-25 11:58:53 +1200

    0:35 into Payout Calculator Part 2 - Video

---
+++ 04_Prize_generation_v3.py
@@ -0,0 +1,40 @@
+# Increased trials and just showed stats.
+
+import random
+
+NUM_TRIALS = 100
+winnings = 0
+
+cost = NUM_TRIALS * 5
+
+for item in range(0, NUM_TRIALS):
+    # prize = ""
+    round_winnings = 0
+
+    for thing in range(0, 3):
+
+        # randint finds numbers between given endpoints, including both endpoints
+        prize_num = random.randint(1,4)
+        # prize += " "
+        if prize_num == 1:
+            # prize += "gold"
+            round_winnings += 5
+        elif prize_num == 2:
+            # prize += "silver"
+            round_winnings += 2
+        elif prize_num == 3:
+            # prize += "copper"
+            round_winnings += 1
+        '''else:
+            prize += "lead"'''
+
+    # print("You won {} which is worth {}".format(prize, round_winnings))
+    winnings += round_winnings
+
+print("Paid In: ${}".format(cost))
+print("Pay Out: ${}".format(winnings))
+
+if winnings > cost:
+    print("You came out ${} ahead".format(winnings - cost))
+else:
+    print("Sorry, you lost ${}".format(cost-winnings))
