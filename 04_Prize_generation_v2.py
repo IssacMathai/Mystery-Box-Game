1c7241a5cb6c690e348a69e76b874da1a55dc3c5
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-25 11:54:36 +1200

    Payout Calculator Part 1 - Video
    End of video.

---
+++ 04_Prize_generation_v2.py
@@ -0,0 +1,44 @@
+# Could not remember if I should use randint or randrange so checked that
+# randint would randomly generate all four of the numbers 1, 2, 3, 4
+
+# If there is an equal chance of winning lead, copper, silver, or gold
+# the house does not have an advantage (although it is close)
+
+import random
+
+NUM_TRIALS = 10
+winnings = 0
+
+cost = NUM_TRIALS * 5
+
+for item in range(0, NUM_TRIALS):
+    prize = ""
+    round_winnings = 0
+
+    for thing in range(0, 3):
+
+        # randint finds numbers between given endpoints, including both endpoints
+        prize_num = random.randint(1,4)
+        prize += " "
+        if prize_num == 1:
+            prize += "gold"
+            round_winnings += 5
+        elif prize_num == 2:
+            prize += "silver"
+            round_winnings += 2
+        elif prize_num == 3:
+            prize += "copper"
+            round_winnings += 1
+        else:
+            prize += "lead"
+
+    print("You won {} which is worth {}".format(prize, round_winnings))
+    winnings += round_winnings
+
+print("Paid In: ${}".format(cost))
+print("Pay Out: ${}".format(winnings))
+
+if winnings > cost:
+    print("You came out ${} ahead".format(winnings - cost))
+else:
+    print("Sorry, you lost ${}".format(cost-winnings))
